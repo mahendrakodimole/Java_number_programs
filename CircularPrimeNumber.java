e2ca92be9f2f9758df2329f946a45ecefaa6d6b6
Mahendra M <97169390+mahendrakodimole@users.noreply.github.com>
2023-05-30 09:13:35 +0000

    check given number is Circular Prime Number or not

--- CircularPrimeNumber.java
+++ CircularPrimeNumber.java
@@ -1,6 +1,9 @@
 public class CircularPrimeNumber {
+    /*A circular prime is a prime number with the property that the number generated at each intermediate step when 
+    cyclically permuting its digits will be prime. 
+    For example, 1193 is a circular prime, since 1931, 9311 and 3119 all are also prime. */
     public static void main(String[] args) {
-       int input =1977;
+       int input =1193;
        String result=isCircularPrime(input);
        System.out.println(input+" "+result);
     }
