8108dcb49c822c86cdbeeb7e56a36c9ea77e5573
Mahendra M <97169390+mahendrakodimole@users.noreply.github.com>
2023-06-04 03:04:23 +0000

    Added Comments

--- Armstrong.java
+++ Armstrong.java
@@ -15,21 +15,21 @@ public static void main(String []args){
         int digits=0;
         while (n>0){
             n/=10;
-            digits++;
+            digits++ ;//3
     }
     return digits;
 }
 public static int sumCubesOfDigits(int input,int digits){
     int sum=0;
-    for(int i=0;i<=digits;i++){
-        int n=input%10;
+    for(int i=1;i<=digits;i++){
+        int n=input%10; //3 ,5
         int value=1;
             for(int j=1;j<=digits;j++){
-            value*=n;
+            value*=n; //1*3 3*3 9*3   1*5 5*5 25*5  1*1 1*1 1*1
             }
-        sum+=value;
-        input/=10;
+        sum+=value; //0+27   27+125  27+125+1
+        input/=10; //15   1
     }
-    return sum;
+    return sum;//153
 }
 }
